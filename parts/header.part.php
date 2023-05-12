f05ad0907a1cd94a708a5be82493020bc1c71d80
MarvinLey <122780349+MarvinLey@users.noreply.github.com>
2023-05-12 17:36:33 +0200

    Update header.part.php

--- parts/header.part.php
+++ parts/header.part.php
@@ -10,9 +10,9 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="bootstrap/style.css">
-    <script src="bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="bootstrap/bootstrap/js/bootstrap.bundle.js"></script>
     <script src="https://kit.fontawesome.com/b3e2eeae95.js" crossorigin="anonymous"></script>
 
     <title>Webentwicklung</title>
